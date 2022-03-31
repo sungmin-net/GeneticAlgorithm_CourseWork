d13801d222ad45c8a0b30946f8bd628c06920a90
Sungmin <sungmin@snu.ac.kr>
2022-04-01 00:01:22 +0900

    update

--- prj1_royalroad/GA1Main.java
+++ prj1_royalroad/GA1Main.java
@@ -1,7 +1,11 @@
+//
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -14,14 +18,18 @@ public class GA1Main {
     private static final int TIMEOUT = 30000; // 30 sec.
 
     // hyper parameters
-    private static int POPULATION_SIZE = 0;
-    private static final double LEAGUE_SELECTION_ROUND = 5;
+    private static final double SELECTION_EXCEPTION = 0.1; // anyone can be a parent with 10%
+    private static final double SELECTION_PRESSURE = 0.1; // if not, upper 10% can be a parent
+    private static final int POPULATION_PARAMETER = 100; // this x mChromosomeLength = pop.size
+
     private static Random mRandom = new Random();
     private static int mN;
     private static String mY;
+    private static int mChromosomeLength = 0; // will be mN * 8
+    private static int mPopulationSize = 0; // will be chromosome length * 100
+    private static int mMaxQuality = 0; // will be 13 * (mN / 2) if mN is even, else,
 
     private static StringBuffer mBuf = new StringBuffer(); // TODO use this.
-
 
     public static void main(String[] args) throws Exception {
 
@@ -32,12 +40,18 @@ public class GA1Main {
         mN = Integer.parseInt(input[ 0 ]);
         mY = input[ 1 ];
 
-        POPULATION_SIZE = 8 * mN * 50;
+        mChromosomeLength = 8 * mN;
+        mPopulationSize = mChromosomeLength * POPULATION_PARAMETER;
+        if ((mN & 1) == 0) {
+            mMaxQuality = 13 * (mN / 2);
+        } else {
+            mMaxQuality = ((13 * mN) + 2) / 2;
+        }
 
         int generation = 1;
-        int totalQuality = 0;
+        int generationTotalQuality = 0;
         Solution bestSolution = null;
-        List<Solution> solutions = new ArrayList<>(POPULATION_SIZE);
+        List<Solution> population = new ArrayList<>(mPopulationSize);
 
         // 1. Generate initial solutions
         long populationStartTime = System.currentTimeMillis();
@@ -45,26 +59,31 @@ public class GA1Main {
         mBuf.append("# Random population started : ").append(populationStartTime);
         System.out.println(mBuf.toString());
 
-        for (int id = 1 ; id <= POPULATION_SIZE ; id++) {
+        for (int id = 1 ; id <= mPopulationSize ; id++) {
             String chromosome = getRandomChromosome(mN);
             int quality = getQuality(chromosome);
             Solution curSolution = new Solution(chromosome, generation, id, quality, null, null);
-            solutions.add(curSolution);
-            totalQuality += quality;
+            population.add(curSolution);
+            generationTotalQuality += quality;
             if (bestSolution == null || bestSolution.mQuality < quality) {
                 bestSolution = curSolution;
             }
-            System.out.println(curSolution);
+//            System.out.println(curSolution);
         }
+        Collections.sort(population, new Comparator<Solution>() {
+            @Override
+            public int compare(Solution s1, Solution s2) {
+                return s2.mQuality - s1.mQuality;
+            }
+        });
 
         long populationEndTime = System.currentTimeMillis();
-
         mBuf.setLength(0);
         mBuf.append("# Random population ended : ").append(populationEndTime);
         mBuf.append(" (takes ").append(populationEndTime - populationStartTime).append("ms)");
+        mBuf.append("\n").append("# Current average: ").append(
+                generationTotalQuality / mPopulationSize);
         mBuf.append("\n").append("# Current best : ").append(bestSolution.toString());
-        mBuf.append("\n").append("# Current quality average: ").append(
-                (double) totalQuality / POPULATION_SIZE);
         System.out.println(mBuf.toString());
 
         // 2. evolution
@@ -74,107 +93,88 @@ public class GA1Main {
         do {
             curGenerationStartTime = System.currentTimeMillis();
             generation++;
-            totalQuality = 0;
-            List<Solution> children = new ArrayList<>(POPULATION_SIZE);
+            generationTotalQuality = 0;
+            List<Solution> children = new ArrayList<>(mPopulationSize);
             mBuf.setLength(0);
             mBuf.append("# Generation ").append(generation).append(" started : ").append(
                     curGenerationStartTime);
             System.out.println(mBuf.toString());
 
-            for (int id = 1 ; id <= POPULATION_SIZE ; id++) {
+            for (int id = 1 ; id <= mPopulationSize ; id++) {
                 // 2-1 select two parent
-                int parentOneIndex = mRandom.nextInt(POPULATION_SIZE);
-                for (int i = 0 ; i < LEAGUE_SELECTION_ROUND ; i++) {
-                    int challengerIdx = mRandom.nextInt(POPULATION_SIZE);
-                    while (challengerIdx == parentOneIndex) {
-                        challengerIdx = mRandom.nextInt(POPULATION_SIZE);
-                    }
-
-                    if (solutions.get(challengerIdx).mQuality
-                            > solutions.get(parentOneIndex).mQuality) {
-                        int temp = parentOneIndex;  // for swap
-                        parentOneIndex = challengerIdx;
-                        challengerIdx = temp;
-                    }
+                int parentOneIndex = mRandom.nextInt(mPopulationSize);
+                if (mRandom.nextDouble() > SELECTION_EXCEPTION) {
+                    parentOneIndex = mRandom.nextInt((int) (mPopulationSize * SELECTION_PRESSURE));
                 }
 
                 // Note. must parent1 != parent2
-                int parentTwoIndex = mRandom.nextInt(POPULATION_SIZE);
+                int parentTwoIndex = mRandom.nextInt(mPopulationSize);
                 while (parentTwoIndex == parentOneIndex) {
-                    parentTwoIndex = mRandom.nextInt(POPULATION_SIZE);
+                    parentTwoIndex = mRandom.nextInt(mPopulationSize);
                 }
 
-                for (int i = 0 ; i < LEAGUE_SELECTION_ROUND ; i++) {
-                    int challengerIdx = mRandom.nextInt(POPULATION_SIZE);
-                    while (challengerIdx == parentTwoIndex || challengerIdx == parentOneIndex) {
-                        challengerIdx = mRandom.nextInt(POPULATION_SIZE);
-                    }
-
-                    if (solutions.get(challengerIdx).mQuality
-                            > solutions.get(parentTwoIndex).mQuality) {
-                        int temp = parentTwoIndex;
-                        parentTwoIndex = challengerIdx;
-                        challengerIdx = temp;
+                if (mRandom.nextDouble() > SELECTION_EXCEPTION) {
+                    while (parentTwoIndex == parentOneIndex) {
+                        parentTwoIndex = mRandom.nextInt(
+                                (int) (mPopulationSize * SELECTION_PRESSURE));
                     }
                 }
 
-                // 2-2. crossover
-                // first, make parent1 is better than parent2
-                if (solutions.get(parentTwoIndex).mQuality
-                        > solutions.get(parentOneIndex).mQuality) {
-                    int temp = parentTwoIndex;
-                    parentTwoIndex = parentOneIndex;
-                    parentOneIndex = temp;
+                // 2-2. crossover - 3pt-crossover
+                int[] cutPoints = new int[ 3 ];
+                cutPoints[0] = mRandom.nextInt(mChromosomeLength);
+                cutPoints[1] = mRandom.nextInt(mChromosomeLength);
+                while (cutPoints[ 1 ] == cutPoints[0]) {
+                    cutPoints[1] = mRandom.nextInt(mChromosomeLength);
+                }
+                cutPoints[2] = mRandom.nextInt(mChromosomeLength);
+                while (cutPoints[ 2 ] == cutPoints[ 0 ] || cutPoints[ 2 ] == cutPoints[ 1 ]) {
+                    cutPoints[ 2 ] = mRandom.nextInt(mChromosomeLength);
                 }
 
-                // then, crossover them like the below.
-                //  |---(p1-1)---|---(p1-2)---|---(p1-3)---|---(p1-4)---|---(p1-5)---|...
-                //  |---(p2-1)---|---(p2-2)---|---(p2-3)---|---(p2-4)---|---(p2-5)---|...
-                //  ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓...
-                //  |---(p1-1)---|---(p2-2)---|---(p1-3)---|---(p2-4)---|---(p1-5)---|...
+                Arrays.sort(cutPoints);
+                mBuf.setLength(0);
+                mBuf.append(population.get(parentOneIndex).mChromosome.subSequence(
+                        0, cutPoints[ 0 ]));
+                mBuf.append(population.get(parentTwoIndex).mChromosome.subSequence(
+                        cutPoints[ 0 ], cutPoints[ 1 ]));
+                mBuf.append(population.get(parentOneIndex).mChromosome.subSequence(
+                        cutPoints[ 1 ], cutPoints[ 2 ]));
+                mBuf.append(population.get(parentTwoIndex).mChromosome.subSequence(
+                        cutPoints[ 2 ], mChromosomeLength));
 
-                String p1chromosome = solutions.get(parentOneIndex).mChromosome;
-                String p2chromosome = solutions.get(parentTwoIndex).mChromosome;
-                boolean isP1turn = true;
-
-                StringBuffer buf = new StringBuffer();
-                for (int i = 0 ; i < mN ; i++) {
-                    if (isP1turn) {
-                        buf.append(p1chromosome.substring(8 * i, 8 * (i + 1)));
-                    } else {
-                        buf.append(p2chromosome.substring(8 * i, 8 * (i + 1)));
-                    }
-                    isP1turn = !isP1turn;
-                }
-
-                String childChromosome = buf.toString();
+                String childChromosome = mBuf.toString();
                 Solution childSolution = new Solution(childChromosome, generation, id,
-                        getQuality(childChromosome), solutions.get(parentOneIndex),
-                        solutions.get(parentTwoIndex));
+                        getQuality(childChromosome), population.get(parentOneIndex),
+                        population.get(parentTwoIndex));
 
-                System.out.println(childSolution);
+//                System.out.println(childSolution);
 
                 if (bestSolution.mQuality < childSolution.mQuality) {
                     bestSolution = childSolution;
                 }
 
-                // remove the worst in the league selection
-                totalQuality += childSolution.mQuality;
+                generationTotalQuality += childSolution.mQuality;
                 children.add(childSolution);
-
             } // end
 
             // 2-3. replacement
-            solutions = children;
+            population = children;
+            Collections.sort(population, new Comparator<Solution>() {
+                @Override
+                public int compare(Solution s1, Solution s2) {
+                    return s2.mQuality - s1.mQuality;
+                }
+            });
             curGenerationEndTime = System.currentTimeMillis();
             long curLaptime = curGenerationEndTime - curGenerationStartTime;
 
             mBuf.setLength(0);
             mBuf.append("# Generation ").append(generation).append(" ended : ").append(
                     curGenerationEndTime).append(" (takes ").append(curLaptime).append("ms)");
-            mBuf.append("\n").append("# Current best : ").append(bestSolution.toString());
             mBuf.append("\n").append("# Current quality average: ").append(
-                    (double) totalQuality / POPULATION_SIZE);
+                    generationTotalQuality / mPopulationSize);
+            mBuf.append("\n").append("# Current best : ").append(bestSolution.toString());
             System.out.println(mBuf.toString());
 
             maxLapTime = Math.max(maxLapTime, curLaptime);
@@ -188,7 +188,8 @@ public class GA1Main {
 
         mBuf.setLength(0);
         mBuf.append("# Done. (takes ").append(System.currentTimeMillis() - START_TIME)
-                .append(" ms, maxLapTime ").append(maxLapTime).append(")");
+                .append(" ms, maxLapTime ").append(maxLapTime).append(" ms, ")
+                .append(bestSolution.mQuality).append("/").append(mMaxQuality).append(")");
         System.out.println(mBuf.toString());
     }
 
